28066ece03431bc9f98fde83b5f26b6647fd1cc1
mujahidmunir <mujahidmunir15@gmail.com>
2023-02-22 18:28:41 +0700

    fix redirect login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -42,9 +42,11 @@ class LoginController extends Controller
     public function authenticated(Request $request, $user)
     {
         if ($user->hasRole('admin')){
-            return redirect('admin');
+            return redirect('admin/report');
         } else {
             return redirect('/');
         }
     }
 }
+
+
