1669f6c7b6ab617aed9cdd04c1e359f062bdfe7c
Rifaldi01 <rifaldipratama2004@gmail.com>
2026-09-03 18:32:36 +0700

    Jadwal kerja

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,38 @@
 @section('title', 'Jadwal Kerja')
 
 @section('content')
+    <div class="card border-0 border-start border-bottom border-5 radius-15 border-secondary">
+        <div class="card-header ">
+            <h3 class="mt-3 mb-3">Today Report</h3>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table id="example" class="table table-striped table-bordered">
+                    <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Action</th>
+                        <th>Title</th>
+                        <th>Category</th>
+                        <th width="10%" class="text-center">Link</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($reports as $key => $data)
+                        <tr>
+                            <td>{{$key+1}}</td>
+                            <td>{{$data->action ? $data->action->name : null}}</td>
+                            <td>{{$data->title}}</td>
+                            <td>{{$data->category->name}}</td>
+                            <td><a href="{{$data->link}}" target="_blank" class="btn btn-sm btn-primary d-grid">view</a> </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
 
+                </table>
+            </div>
+        </div>
+    </div>
     <div class="container-fluid">
 
         {{-- ========================================================= --}}
